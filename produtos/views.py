16848c603d28be0f2b57766091a91f6a3d626143
MatheusCacemiro <matheus.cacemiro@gmail.com>
2023-09-27 16:54:35 -0300

    Removido 5 imagens no index e colocado uma imagem em uma estrutura for

--- produtos/views.py
+++ produtos/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+
+
 def index(request):
-    return render(request, 'produtos/index.html')
+    
+    dados = {
+
+    1: {"nome": "Coxinha",
+        "legenda": "Coxinha deliciosa feita na hora"},
+    2: {"nome": "Donuts",
+        "legenda": "Donuts de vários sabores"}
+}
+
+    return render(request, 'produtos/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'produtos/imagem.html')
