58ace3c2fec61fe47128aa24695bef8ab8edee40
MatheusCacemiro <matheus.cacemiro@gmail.com>
2023-09-30 17:25:58 -0300

    criado modelo de banco de dados com sqlite3 do Django para tornar as referências mais dinâmincas

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+     nome = models.CharField(max_length=100, null=False, blank=False)
+     legenda = models.CharField(max_length=100, null=False, blank=False)
+     descricao = models.TextField(null=False, blank=False)
+     item = models.CharField(max_length=100, null=False, blank=False)
+
+     def __str__(self):
+          return f"Produto [nome={self.nome}]"
--- produtos/views.py
+++ produtos/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
 
-
+from produtos.models import Produto
 
 def index(request):
-    
-    dados = {
-
-    1: {"nome": "Coxinha",
-        "legenda": "Coxinha deliciosa feita na hora"},
-    2: {"nome": "Donuts",
-        "legenda": "Donuts de vários sabores"}
-}
-
-    return render(request, 'produtos/index.html', {"cards": dados})
+    produtos = Produto.objects.all()    
+    return render(request, 'produtos/index.html', {"cards": produtos})  # Passa todos os itens do banco de dados para o index.html
 
 def imagem(request):
     return render(request, 'produtos/imagem.html')
